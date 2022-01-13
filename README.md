173d38dc316290291ed1a1f697f4ae7924d01dc8
Robin Rhodes <bigr0b@protonmail.com>
2022-01-12 22:21:29 -0500

    added pictures for what the computer and player selected

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+The Odin Project
 Making a simple rock paper scissors program
+Added a responsive UI to it with javascript and css
+using the skills learned to up to section 10
--- app.js
+++ app.js
@@ -3,43 +3,75 @@ let computerScore = 0;
 let playerScore = 0;
 let tieScore = 0;
 let output = ""
+let pictureBoard = document.getElementById("pictureBoard");
+
 //computers pick
 function computerPlay() {
     rockPaperScissors = Math.floor(Math.random() * 3) + 1;
     if (rockPaperScissors === 1) {
-        return "Rock";
+        return "rock";
     }
     if (rockPaperScissors === 2) {
-        return "Paper";
+        return "paper";
     }
     else{
-        return "Scissors";
+        return "scissors";
     }
 }
-
+// Scoreboard Pictures
+function computerPicture(computerRPS){
+    let computerPic = document.getElementById("computerPic");
+    if (computerRPS === "rock") {
+        computerPic.src = "images/rock.jpg";
+        pictureBoard.appendChild(computerPic); 
+    }
+    if (computerRPS === "paper") {
+        computerPic.src = "images/paper.jpg";
+        pictureBoard.appendChild(computerPic); 
+    }
+    if (computerRPS === "scissors")  {
+        computerPic.src = "images/scissors.jpg";
+        pictureBoard.appendChild(computerPic); 
+    }
+}
+function playerPicture(playerRPS){
+    let playerPic = document.getElementById("playerPic");
+    if (playerRPS === "rock") {
+        playerPic.src = "images/rock.jpg";
+        pictureBoard.appendChild(computerPic); 
+    }
+    if (playerRPS === "paper") {
+        playerPic.src = "images/paper.jpg";
+        pictureBoard.appendChild(computerPic); 
+    }
+    if (playerRPS === "scissors")  {
+        playerPic.src = "images/scissors.jpg";
+        pictureBoard.appendChild(computerPic); 
+    }
+}
 function playRound(playerSelection, computerSelection) {
     if (playerScore < 5 && computerScore < 5) {
-        if(playerSelection === "rock" && computerSelection === "Paper"){
+        if(playerSelection === "rock" && computerSelection === "paper"){
             computerScore += 1;
             return "You Lose! Paper beats Rock";
         }
-        if(playerSelection === "paper" && computerSelection === "Scissors"){
+        if(playerSelection === "paper" && computerSelection === "scissors"){
             computerScore += 1;
             return "You Lose! Scissors beats Paper";
         }        
-        if(playerSelection === "scissors" && computerSelection === "Rock"){
+        if(playerSelection === "scissors" && computerSelection === "rock"){
             computerScore += 1;
             return "You Lose! Rock beats Scissors";
         }        
-        if(playerSelection === "rock" && computerSelection === "Scissors"){
+        if(playerSelection === "rock" && computerSelection === "scissors"){
             playerScore += 1;
             return "You Win! Rock beats Scissors";
         }
-        if(playerSelection === "paper" && computerSelection === "Rock"){
+        if(playerSelection === "paper" && computerSelection === "rock"){
             playerScore += 1;
             return "You Win! Paper beats Rock";
         }        
-        if(playerSelection === "scissors" && computerSelection === "Paper"){
+        if(playerSelection === "scissors" && computerSelection === "paper"){
             playerScore += 1;
             return "You Win! Scissors beats Paper";
         }
@@ -61,6 +93,7 @@ function gameOver() {
     }
 }
 
+//button click event listener
 const rpsButtons = document.querySelectorAll(".rpsButton");
 rpsButtons.forEach(rpsButton => {
     rpsButton.addEventListener("click", (e) => {
@@ -70,12 +103,14 @@ rpsButtons.forEach(rpsButton => {
         document.getElementById("player").innerHTML = playerScore;
         document.getElementById("computer").innerHTML = computerScore;
         document.getElementById("tie").innerHTML = tieScore;
+        pictureBoard.classList.remove("hidden");
+        computerPicture(comp);
+        playerPicture(play);
         if (playerScore > 4 || computerScore > 4){
             document.getElementById("result").innerHTML = gameOver();
         }
     })
 })
-
 
 //reset button
 function reset() {
@@ -87,6 +122,7 @@ function reset() {
     document.getElementById("tie").innerHTML = tieScore
     document.getElementById("result").innerHTML = "";
     resetButton.classList.add("hidden");
+    pictureBoard.classList.add("hidden");
 }
 
 const resetButton = document.getElementById("reset");
